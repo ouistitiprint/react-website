3ea230da6b04a164052172d54208c42627a8e515
Lucas Vandroux <lucas.vandroux@gmail.com>
2020-07-14 11:48:08 +0200

    Updates

---
+++ precache-manifest.c1c980ab56ddc1b6eb28b9a91ff7b5b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9349d6b2bac1372fa4fa0e078a9ea7",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "aac2f305fba468ece86d",
+    "url": "/react-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b3034f36d922717b6667",
+    "url": "/react-website/static/js/2.f1becb4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-website/static/js/2.f1becb4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aac2f305fba468ece86d",
+    "url": "/react-website/static/js/main.13d14934.chunk.js"
+  },
+  {
+    "revision": "fadfad2d28377605b9ac",
+    "url": "/react-website/static/js/runtime-main.caee497b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
