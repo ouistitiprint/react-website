db94864eff1023c6f9247ccebb882a799eb9966e
Lucas Vandroux <lucas.vandroux@gmail.com>
2020-07-18 19:16:02 +0200

    Add Perks description

--- src/data/campaign.ts
+++ src/data/campaign.ts
@@ -16,6 +16,7 @@ export interface ICampaignData {
     startDate: Date,
     endDate: Date,
     description: string,
+    perksDescription: string,
     artworks:artworks.IArtworksData[],
     artists:artists.IArtistsData[],
 }
@@ -34,6 +35,7 @@ export const blm: ICampaignData =
         startDate: new Date(2020, 6, 1, 0, 0, 0, 0),
         endDate: new Date(2020, 6, 31, 23, 59, 59, 0),
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rhoncus tortor eget erat euismod feugiat non sit amet quam. Quisque vel nisl sit amet metus semper laoreet. Sed venenatis hendrerit lorem, sed scelerisque tortor accumsan non. Phasellus efficitur turpis sed molestie mollis. Ut erat ex, congue ac dignissim nec, facilisis eu metus. Mauris risus dolor, vehicula eget dapibus nec, scelerisque ut dolor. Donec lacinia diam sed risus condimentum, a efficitur quam ultricies. Suspendisse potenti. Duis pellentesque purus ac ullamcorper interdum.",
+        perksDescription: "This project is a little bit different from our previous project and as we decided to feature different black personalities, we divided each section per personality.",
         artworks:[artworks.kobeBryant, artworks.oluwaytoyinSalau, artworks.camilleAumontCarnel, artworks.jaeAdrianaParmer, artworks.amandineGay, artworks.blmStickers, artworks.blmCards],
         artists:[artists.priya, artists.johari],
     };
