7f657f18536f48e291dddabe1911fb4b7e3a53f1
Lucas Vandroux <lucas.vandroux@gmail.com>
2020-07-19 19:55:17 +0200

    Breakdown value of perk between good, shipping and donation.

--- src/data/perks.ts
+++ src/data/perks.ts
@@ -2,7 +2,11 @@ export interface IPerkData {
     name: string,
     type: "tote" | "poster" | "stickers" | "cards",
     description: string,
-    price: number,
+    value: {
+        good: number,
+        shipping: number,
+        donation: number,
+    },
     groupArtworks: boolean,
 }
 
@@ -10,7 +14,11 @@ export const tote: IPerkData = {
     name: "Tote Bag",
     type: "tote",
     description: "The perfect tote bag to show your support when going outside.",
-    price: 20,
+    value: {
+        good: 20,
+        shipping: 0,
+        donation: 5,
+    },
     groupArtworks: false,
 }
 
@@ -18,7 +26,11 @@ export const poster: IPerkData = {
     name: "A4 Poster",
     type: "poster",
     description: "The perfect poster to remind us and the world that the fight is important.",
-    price: 30,
+    value: {
+        good: 30,
+        shipping: 0,
+        donation: 10,
+    },
     groupArtworks: false,
 }
 
@@ -26,7 +38,11 @@ export const stickers: IPerkData = {
     name: "5x Stickers",
     type: "stickers",
     description: "The perfect stickers to show your support.",
-    price: 15,
+    value: {
+        good: 15,
+        shipping: 0,
+        donation: 3,
+    },
     groupArtworks: true,
 }
 
@@ -34,6 +50,10 @@ export const cards: IPerkData = {
     name: "Complete Collection",
     type: "cards",
     description: "All the artworks of the campaign printed on high quality cards with a matte finish.",
-    price: 8,
+    value: {
+        good: 8,
+        shipping: 0,
+        donation: 2,
+    },
     groupArtworks: true,
 }
