d32a42f636dcd4d76e2413e3f05312d940522dfe
Lucas Vandroux <lucas.vandroux@gmail.com>
2020-07-19 19:39:31 +0200

    Dialog to display the PerkShop

---
+++ src/views/DialogPerkShop.tsx
@@ -0,0 +1,68 @@
+import React, { Dispatch, SetStateAction } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { IPerkCampaign } from "../data/campaign";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import PerkShop from "../presenters/PerkShop";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from '@material-ui/icons/Close';
+import { Grid, Typography, Slide } from "@material-ui/core";
+import { TransitionProps } from "@material-ui/core/transitions/transition";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+    },
+}));
+
+const Transition = React.forwardRef(function Transition(
+    props: TransitionProps & { children?: React.ReactElement },
+    ref: React.Ref<unknown>,
+  ) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
+
+export interface IDialogPerkShop {
+    perk: IPerkCampaign,
+    showShop: boolean,
+    setShowShop: Dispatch<SetStateAction<boolean>>;
+};
+
+const DialogPerkShop: React.FC<IDialogPerkShop> = ({ perk, showShop, setShowShop }) => {
+    const classes = useStyles();
+
+    return (
+        <Dialog onClose={() => setShowShop(false)} open={showShop} TransitionComponent={Transition} fullScreen>
+            <DialogTitle disableTypography>
+                <Grid container
+                    direction="row"
+                    justify="space-between"
+                    alignItems="center">
+                    <Grid item xs={11}>
+                        <Typography variant="h4" component="h2">
+                            {"Checkout"}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                        <IconButton aria-label="close" onClick={() => setShowShop(false)}>
+                            <CloseIcon />
+                        </IconButton>
+                    </Grid>
+                </Grid>
+
+
+
+            </DialogTitle>
+            <DialogContent dividers>
+                <PerkShop perk={perk} />
+            </DialogContent>
+        </Dialog>
+
+    );
+};
+
+export default DialogPerkShop;
