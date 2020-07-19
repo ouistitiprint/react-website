7602362a4b7c62a6b4fd2419bc64eec751daa5f0
Lucas Vandroux <lucas.vandroux@gmail.com>
2020-07-19 13:01:38 +0200

    Add type to perk to easily identify the mockups

--- src/data/perks.ts
+++ src/data/perks.ts
@@ -1,33 +1,38 @@
 export interface IPerkData {
     name: string,
+    type: "tote" | "poster" | "stickers" | "cards",
     description: string,
     price: number,
     groupArtworks: boolean,
 }
 
 export const tote: IPerkData = {
-    name: "tote",
+    name: "Tote Bag",
+    type: "tote",
     description: "The perfect tote bag to show your support when going outside.",
     price: 20,
     groupArtworks: false,
 }
 
 export const poster: IPerkData = {
-    name: "poster",
+    name: "A4 Poster",
+    type: "poster",
     description: "The perfect poster to remind us and the world that the fight is important.",
     price: 30,
     groupArtworks: false,
 }
 
 export const stickers: IPerkData = {
-    name: "stickers",
+    name: "6x Stickers",
+    type: "stickers",
     description: "The perfect stickers to show your support.",
     price: 15,
     groupArtworks: true,
 }
 
 export const cards: IPerkData = {
-    name: "cards",
+    name: "11x Cards",
+    type: "cards",
     description: "The smallest support but the only one with the complete collection of cards.",
     price: 8,
     groupArtworks: true,
