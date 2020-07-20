833c4589f79cc2bce4e155ef9fdf081b283c436c
Lucas Vandroux <lucas.vandroux@gmail.com>
2020-07-20 22:50:34 +0200

    Clean queryString from untype and unused function

--- src/helpers/queryString.ts
+++ src/helpers/queryString.ts
@@ -1,5 +1,4 @@
 import qs from "query-string";
-import { useState, useCallback } from "react";
 
 // Modify the code from this article: https://medium.com/swlh/using-react-hooks-to-sync-your-component-state-with-the-url-query-string-81ccdfcb174f
 
@@ -11,14 +10,6 @@ const setQueryStringWithoutPageReload = (qsValue:string) => {
     window.location.pathname +
     qsValue;
   window.history.pushState({ path: newurl }, "", newurl);
-};
-
-export const getQueryStringValue = (
-  key:string,
-  queryString = window.location.search
-) => {
-  const values = qs.parse(queryString);
-  return values[key];
 };
 
 export const getPaymentStatus = (
@@ -50,24 +41,3 @@ export const setQueryStringValue = (
   });
   setQueryStringWithoutPageReload(`?${newQsValue}`);
 };
-
-function useQueryString(key:string, initialValue?:string) {
-    const [value, setValue] = useState(getQueryStringValue(key) || initialValue);
-    // const onSetValue = useCallback(
-    //   newValue => {
-    //     setValue(newValue);
-    //     setQueryStringValue(key, newValue);
-    //   },
-    //   [key]
-    // );
-    
-    // const onSetValue = (newValue: string) => {
-    //     setValue(newValue);
-    //     setQueryStringValue(key, newValue);
-    // }
-  
-    // return [value, onSetValue];
-    return [value, setValue];
-  }
-
-export default useQueryString;
