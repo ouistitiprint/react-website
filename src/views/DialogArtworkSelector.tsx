0c1d713d9ec69f8a3734bc9b67d33ab39f87967f
Lucas Vandroux <lucas.vandroux@gmail.com>
2020-07-20 22:58:40 +0200

    Cleaned up and format DialogArtworkSelector

--- src/views/DialogArtworkSelector.tsx
+++ src/views/DialogArtworkSelector.tsx
@@ -1,11 +1,9 @@
 import React, { Dispatch, SetStateAction } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
-import { IPerkCampaign, IFoundationData } from "../data/campaign";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
-import PerkShop from "../presenters/PerkShop";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from '@material-ui/icons/Close';
 import { Grid, Typography, Slide, Container } from "@material-ui/core";
@@ -15,18 +13,16 @@ import ArtworkButton from "../controls/ArtworkButton";
 
 const useStyles = makeStyles(theme => ({
     root: {
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
+
     },
 }));
 
 const Transition = React.forwardRef(function Transition(
     props: TransitionProps & { children?: React.ReactElement },
     ref: React.Ref<unknown>,
-  ) {
+) {
     return <Slide direction="up" ref={ref} {...props} />;
-  });
+});
 
 export interface IDialogArtworkSelector {
     artworks: IArtworksData[],
@@ -44,42 +40,40 @@ const DialogArtworkSelector: React.FC<IDialogArtworkSelector> = ({ artworks, set
     };
 
     return (
-        <Dialog onClose={() => setShowSelector(false)} open={showSelector} TransitionComponent={Transition} fullScreen>
+        <Dialog onClose={() => setShowSelector(false)} open={showSelector} TransitionComponent={Transition} fullScreen className={classes.root}>
             <DialogTitle disableTypography>
                 <Container disableGutters>
-                <Grid container
-                    direction="row"
-                    justify="space-between"
-                    alignItems="center">
-                    <Grid item xs={11}>
-                        <Typography variant="h4" component="h2">
-                            {"Select"}
-                        </Typography>
+                    <Grid container
+                        direction="row"
+                        justify="space-between"
+                        alignItems="center">
+                        <Grid item xs={11}>
+                            <Typography variant="h4" component="h2">
+                                {"Select"}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={1}>
+                            <IconButton aria-label="close" onClick={() => setShowSelector(false)}>
+                                <CloseIcon />
+                            </IconButton>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={1}>
-                        <IconButton aria-label="close" onClick={() => setShowSelector(false)}>
-                            <CloseIcon />
-                        </IconButton>
-                    </Grid>
-                </Grid>
                 </Container>
             </DialogTitle>
             <DialogContent dividers>
                 <Grid container
-                direction="row"
-                spacing={1}>
-                {artworks.map((artwork) => {
-                    return (
-                        <Grid item xs={12} md={6} lg={4}>
-                        <ArtworkButton artwork={artwork} handleClick={() => handleArtworkSelection(artwork)}/>
-                        </Grid>
-                    );
-                })}
+                    direction="row"
+                    spacing={1}>
+                    {artworks.map((artwork) => {
+                        return (
+                            <Grid item xs={12} md={6} lg={4}>
+                                <ArtworkButton artwork={artwork} handleClick={() => handleArtworkSelection(artwork)} />
+                            </Grid>
+                        );
+                    })}
                 </Grid>
-                
             </DialogContent>
         </Dialog>
-
     );
 };
 
